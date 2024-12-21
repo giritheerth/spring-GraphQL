bfbd8de26b1c35e50172ead05fc81e3d9a9257e2
Theerthagiri Soundararajan <49114091+giritheerth@users.noreply.github.com>
2024-12-21 12:41:07 +1100

    init

---
+++ src/main/java/com/learn/sgraphql/SgraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.learn.sgraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgraphqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/sgraphql/SgraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.sgraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
